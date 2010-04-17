571ba086b3a9efbcd34417792f1adf9dd6dbafe2
Álvaro Castro-Castilla <alvaro@castrocastilla.com>
2010-04-17 18:08:50 +0200

    Basic macros

--- utils/misc.scm
+++ utils/misc.scm
@@ -12,26 +12,30 @@
 
 ;(compile-options cc-options: "-w" force-compile: #t)
 
-;; 0.0-1.0 range to u8 integer
-;;
+;-------------------------------------------------------------------------------
+; Miscelaneous procedures
+;-------------------------------------------------------------------------------
+
+;;; 0.0-1.0 range to u8 integer
+
 (define (normalized-inexact->integer value)
   (modulo (inexact->exact (round (* 255 value))) 255))
 
-;; snoc
-;;
+;;; snoc
+
 (define snoc
   (lambda (ls x)
     (append ls (list x))))
 
-;; Take the first one of a list, if is not a list, take the element itself
-;;
+;;; Take the first one of a list, if is not a list, take the element itself
+
 (define (first-or-element list-or-element)
   (if (list? list-or-element)
       (car list-or-element)
     (list-or-element)))
 
-;; Rotates the list until the first one satisfies the predicate
-;;
+;;; Rotates the list until the first one satisfies the predicate
+
 (define (rotate-until-first pred lis)
   (define (iter lis-iter n)
     (let ((x (car lis-iter))
@@ -45,19 +49,19 @@
         (iter (append (cdr lis-iter) (list x)) (+ n 1))))))
   (iter lis 0))
 
-;; Call/cc with one extra argument
-;;
+;;; Call/cc with one extra argument
+
 (define (call/cc1 procedure value)
   (call/cc (lambda (k) (procedure k value))))
 
-;; atom?
-;;
+;;; atom?
+
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
-;; Recursive map
-;;
+;;; Recursive map
+
 (define (map* f lis)
   (cond
    ((null? lis)
@@ -66,4 +70,31 @@
     (f lis))
    (else
     (cons (map* f (car lis)) (map* f (cdr lis))))))
+
+;-------------------------------------------------------------------------------
+; Macro procedures
+;-------------------------------------------------------------------------------
+
+;;; Syntax error macro
+
+(define-syntax syntax-error
+  (syntax-rules ()
+    ((syntax-error) (syntax-error "Bad use of syntax error!"))))
+
+;;; When
+
+(define-syntax when
+  (syntax-rules ()
+    ((when condition form . forms)
+     (if condition (begin form . forms) #f))))
+
+;;; Extract only the nth-value from a function returning multiple values
+
+(define-syntax nth-value
+  (syntax-rules ()
+    ((nth-value n values-producing-form)
+     (call-with-values
+       (lambda () values-producing-form)
+       (lambda all-values
+         (list-ref all-values n))))))
 
