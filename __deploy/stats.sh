1926064e30d3e5052bc66bf6d0e1ed4a7e19376d
Álvaro Castro-Castilla <alvaro@castrocastilla.com>
2010-05-12 18:37:20 +0200

    Something left out from last commit

--- __deploy/stats.sh
+++ __deploy/stats.sh
@@ -3,4 +3,4 @@
 PATH_TO_SCRIPT=`dirname $0`
 
 cd $PATH_TO_SCRIPT
-cloc ../*.scm ../strategies/*.scm ../fields/*.scm ../utils/*.scm ../geometry/*.scm ../math/*.scm ../ds/*.scm
+cloc ../*.scm ../strategies/*.scm ../fields/*.scm ../core/*.scm ../geometry/*.scm ../math/*.scm ../ds/*.scm
